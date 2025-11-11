f976cebb0a4f2468e986210b8a1f6f2d4e387c08
L Shaf <lshaf@users.noreply.github.com>
2025-11-11 11:00:47 +0700

    📖 finalize website

--- website/app/install/page.js
+++ website/app/install/page.js
@@ -1,11 +1,13 @@
 import PageBanner from "@/components/PageBanner";
 import GlitcheLayout from "@/layouts/GlitcheLayout";
 import "@css/template-dark/dark.css";
+import FlashFirmware from "@/components/FlashFirmware";
 
 const Resume = () => {
   return (
     <GlitcheLayout dark={true}>
       <PageBanner pageLink={"install"} pageName={"Install"} dark={true} />
+      <FlashFirmware />
     </GlitcheLayout>
   );
 };
